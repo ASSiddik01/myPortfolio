e69075b4dbcf272e9578fee23384682da504e7dd
Shama <assiddik001@gmail.com>
2022-05-31 20:18:24 +0600

    install daisy UI and react router

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#ad243e",
+
+          secondary: "#ea8073",
+
+          accent: "#ffcda3",
+
+          neutral: "#18182F",
+
+          "base-100": "#FFFFFF",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
