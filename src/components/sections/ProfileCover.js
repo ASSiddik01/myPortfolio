82952c6ded5cb06bfdbe1d28bdab4ffd2fcf8133
Shama <assiddik001@gmail.com>
2022-05-31 20:57:41 +0600

    profile cover

--- src/components/sections/ProfileCover.js
+++ src/components/sections/ProfileCover.js
@@ -1,9 +1,14 @@
 import React from "react";
+import cover from "../../images/profile_banner.png";
 
 const ProfileCover = () => {
   return (
-    <div>
-      <h2>ProfileCover</h2>
+    <div className="h-60 w-full">
+      <img
+        src={cover}
+        alt="cover"
+        className="w-full h-full object-cover lg:object-center object-left"
+      />
     </div>
   );
 };
