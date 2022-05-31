ed8f635fdf14d2f058d962bbf9c595d85c4878eb
Shama <assiddik001@gmail.com>
2022-05-31 22:49:49 +0600

    footer

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -1,10 +1,25 @@
 import React from "react";
 
 const Footer = () => {
+  // Generate dynamic date
+  const date = new Date().getFullYear();
   return (
-    <div>
-      <h2>Footer</h2>
-    </div>
+    <footer className="bg-white p-4 absolute bottom-0 left-0 w-full">
+      <div className="container px-4">
+        <p className="text-sm text-gray-400 text-center">
+          Copyright @ {date}. All rights reserved. Designed and Developed by
+          <a
+            target="blank"
+            className="text-primary"
+            href="https://imshama.com/"
+            rel="nofollow"
+          >
+            {" "}
+            Abu Shama
+          </a>
+        </p>
+      </div>
+    </footer>
   );
 };
 
