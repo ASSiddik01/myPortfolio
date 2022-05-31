3972449de4250cbf13a2fd935a82ea20507d8a9e
Shama <assiddik001@gmail.com>
2022-05-31 20:37:59 +0600

    page setup and add some image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
+import ProfileCover from "./components/sections/ProfileCover";
+import Sidebar from "./components/sections/Sidebar";
+import Navbar from "./components/sections/Navbar";
+import Footer from "./components/sections/Footer";
 
 function App() {
   return (
-    <div>
-      <button class="inline-block cursor-pointer rounded-md bg-primary px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-secondary">
-        Button
-      </button>
-    </div>
+    <main className="min-h-screen relative bg-gray-50 pb-10">
+      <ProfileCover />
+      <div className="container px-4">
+        <div className="flex flex-wrap px-4">
+          <div className="w-full lg:w-1/3 ">
+            <Sidebar />
+          </div>
+          <div className="w-full lg:w-2/3 ">
+            <Navbar />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </main>
   );
 }
 
---
+++ src/components/sections/ProfileCover.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfileCover = () => {
+  return (
+    <div>
+      <h2>ProfileCover</h2>
+    </div>
+  );
+};
+
+export default ProfileCover;
